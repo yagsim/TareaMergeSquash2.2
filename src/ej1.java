faec49df9fec900bd52b199ea681930f6950d689
yagsim <yagosimofernandez@gmail.com>
2022-03-16 11:50:08 +0100

    commit 1

---
+++ src/ej1.java
@@ -0,0 +1,8 @@
+public class ej1 {
+    public static void main(String[] args) {
+        System.out.println("Share on GitHub");
+        System.out.println("ej1");
+        System.out.println("readme");
+        
+    }
+}
